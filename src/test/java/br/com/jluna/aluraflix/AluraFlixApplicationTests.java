c90afa1351602b4146139f24d678f8776a4fcd04
Josafá Luna <josafaluna@gmail.com>
2021-07-25 11:47:14 -0300

    Iniciando o projeto - AluraFlix - Alura Challenge Back-End

---
+++ src/test/java/br/com/jluna/aluraflix/AluraFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jluna.aluraflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
